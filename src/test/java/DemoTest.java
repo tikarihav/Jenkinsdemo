11c7bb2432aabcd436093f7e01305723265a7001
tikarihav <tikarihav@gmail.com>
2023-02-14 12:37:10 +0530

    added test

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -13,7 +13,7 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 
 public class DemoTest {
      @Test
-     public void evaluatesExpression() {
+     public void testOne() {
     	 try (Playwright playwright = Playwright.create()) {
              Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5));
              Page page = browser.newPage();
@@ -22,6 +22,19 @@ public class DemoTest {
              page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("D:\\playwright.png")));
              assertThat(page).hasTitle(Pattern.compile("dnb-stg - Sign In"));
          }
-        
+        //assertEquals(6, sum);
+     }
+     
+     @Test
+     public void testTwo() {
+    	 try (Playwright playwright = Playwright.create()) {
+             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(5));
+             Page page = browser.newPage();
+             page.navigate("http://stg.sso.dnb.com");
+             page.locator("#idp-discovery-username").fill("fs18regression@dnbiemailtest.org");
+             page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get("D:\\playwright1.png")));
+             assertThat(page).hasTitle(Pattern.compile("Dun and Bradstreet"));
+         }
+        //assertEquals(6, sum);
      }
 }
